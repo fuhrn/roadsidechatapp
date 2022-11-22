e3aa5bfb92865b8b7812f0d99c07808001dcca64
Nestor Santiago Fuhr <nestor.fuhr@gmail.com>
2022-11-22 09:39:12 -0300

    message API

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv")
 const { chats } = require("./data/data");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
